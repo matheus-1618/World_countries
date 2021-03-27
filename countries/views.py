1f797fddd377c873cddb3137275a64bd5c2d5bfe
matheus-1618 <matheus1207live@gmail.com>
2021-03-27 13:53:13 -0300

    Changing the parse

--- countries/views.py
+++ countries/views.py
@@ -10,6 +10,26 @@ from rest_framework.decorators import api_view
 # Create your views here.
 
 #setting the decorator
+@api_view(['GET','POST'])
+def countries_list(request):
+    if request.method=='GET':
+        countries= Countries.objects.all()
+
+        name = request.GET('name',None)
+        if name is not None:
+            countries = countries.filter(name_icontains=name)
+
+        countries_serializer= CountriesSerializer(countries,many=True)
+        return JsonResponse(countries_serializer.data,safe=False)
+
+    elif request.method =='POST':
+        countries_data=JSONParser().parse(request)
+        countries_serializer= CountriesSerializer(data=countries_data)
+        if countries_serializer.is_valid():
+            countries_serializer.save()
+            return JsonResponse(countries_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(countries_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 @api_view(['GET','PUT','DELETE'])
 def countries_detail(request,pk):
     try:
@@ -32,11 +52,4 @@ def countries_detail(request,pk):
     elif request.method == 'DELETE':
         countries.delete()
         return JsonResponse({'message':'Country was deleted sucessfully!'},status=status.HTTP_204_NO_CONTENT)
-        
 
-
-
-
-
-
-    
