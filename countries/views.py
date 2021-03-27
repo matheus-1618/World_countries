3f1516a539d14bc200eb613d4b25fdda7cabe5c0
matheus-1618 <matheus1207live@gmail.com>
2021-03-27 11:48:10 -0300

    Setting admin configurations

--- countries/views.py
+++ countries/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from dajngo.http.reponse import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+from countries.models import Countries
+from countries.serializers import CountriesSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
