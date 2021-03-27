610256ceb575d35fc7438217239930d9bc43528e
matheus-1618 <matheus1207live@gmail.com>
2021-03-27 10:58:18 -0300

    Defining the first model (emulating a database)

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+#Creating our first model
+class Countries(models.Model):
+    name= models.CharField(max_lenght=50,blank=False,default='')
+    capital= models.CharField(max_lenght=50,blank=False,default='')
+
+    class Meta:
+        ordering=('id',)
