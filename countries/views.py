f3879009e2c50c6f4db48671c6381a50381838e2
matheus-1618 <matheus1207live@gmail.com>
2021-03-27 13:28:18 -0300

    First applying of Views

--- countries/views.py
+++ countries/views.py
@@ -8,3 +8,24 @@ from countries.serializers import CountriesSerializer
 from rest_framework.decorators import api_view
 
 # Create your views here.
+
+#setting the decorator
+@api_view('GET','POST')
+def countries_list(request):
+    if request.method=='GET':
+        countries= Countries.objects.all()
+
+        name = request.GET('name',None)
+        if name is not None:
+            countries = countries.folter(name_icontains=name)
+
+        countries_serializer= CountriesSerializer(countries,many=True)
+        return JsonResponse(countries_serializer.data,safe=False)
+
+    elif request.method =='POST':
+        countries_data=JSONParser().parse(request)
+        countries_serializer= CountriesSerializer(data=countries_data)
+        if countries_serializer.is_valid():
+            countries_serializer.save()
+            return JsonResponse(countries_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(countries_serializer.erros, status=status.HTTP_400_BAD_REQUEST)
