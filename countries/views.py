a292a3c4bdefc231730212ce24f13ed1f120e9b4
matheus-1618 <matheus1207live@gmail.com>
2021-03-28 16:29:29 -0300

    Registering the first model

--- countries/views.py
+++ countries/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from dajngo.http.reponse import JsonResponse
+from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 
